20675e931c0ca571fc4c65bb3109ee7f3cf24854
Mahmoud Abdelhady <eng.mahmoud.sh.77@gmail.com>
2024-10-26 16:27:33 +0300

    Initial Commit

---
+++ e-commerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  e-commerce
+//
+//  Created by Mahmoud Elsharkawy on 26/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ e-commerce/e_commerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_commerceApp.swift
+//  e-commerce
+//
+//  Created by Mahmoud Elsharkawy on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct e_commerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
